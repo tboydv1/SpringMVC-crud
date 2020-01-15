75e09c1498aad7782b150019750026c9332c8a78
tboydv1 <tboydv1@gmail.com>
2020-01-15 15:37:43 +0100

    Adding jackson to the classpath for the web project

--- springDemo_web/src/main/java/com/springdemo/contoller/employee/EmployeeController.java
+++ springDemo_web/src/main/java/com/springdemo/contoller/employee/EmployeeController.java
@@ -1,17 +1,16 @@
 package com.springdemo.contoller.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springdemo.employee.service.EmployeeService;
 import com.springdemo.entities.Employee;
 
-@Controller
+@RestController
 public class EmployeeController {
 
 	
@@ -19,11 +18,10 @@ public class EmployeeController {
 	private EmployeeService employeeServiceImpl;
 	
 	
-	
 	@PostMapping("/addEmployee")
-	public String addEmployee(@ModelAttribute("employee") Employee tempEmployee ) {
+	public String addEmployee(@RequestBody Employee tempEmployee) {
 		
-		
+
 		System.out.println(tempEmployee.getEmail());
 		
 		employeeServiceImpl.addEmployee(tempEmployee);
